f15725d161e8f0d53fdfe05174598f2f307bff9e
kdnic1 <mbudzevi@mail.usf.edu>
2021-12-29 15:09:32 -0500

    set1

--- hello.cpp
+++ hello.cpp
@@ -4,14 +4,7 @@ int main()
 {
   printf("%s\n", "Hello Word!");
   cout<<""<< endl ;
-  for (size_t i = 0; i < count; i++) {
-    /* code */
-  }
-  if (/* condition */) {
-    /* code */
-  }
-  while (`/* condition */`) {
-    /* code */
-  }
+
+
   return 0;
 }
